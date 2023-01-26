684d3cc0419e8e54ecb822e3f43385847f506360
Snake <per.mortis.non.lates@gmail.com>
2023-01-26 19:19:11 +0500

    comleted

---
+++ Task13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. (Цифры считать справа налево).
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int num = number / 100;
+if (num==0)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    num = num % 10;
+    Console.WriteLine(num);
+}
