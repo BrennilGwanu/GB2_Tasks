634e17bbc1005192025f30abcf52954e76cfe10a
Snake <per.mortis.non.lates@gmail.com>
2023-01-26 19:20:37 +0500

    comleted

---
+++ Task15/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Clear();
+Console.WriteLine("Введите число дня недели: ");
+int day = int.Parse(Console.ReadLine()!);
+if (day==6||day==7)
+{
+    Console.WriteLine("ДА!");
+}
+else
+Console.WriteLine("Нет");
