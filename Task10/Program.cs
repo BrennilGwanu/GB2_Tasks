cce6084c370a84c38960f3c88224d29011503689
Snake <per.mortis.non.lates@gmail.com>
2023-01-26 19:17:32 +0500

    comleted

---
+++ Task10/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+Console.Clear();
+Console.WriteLine("Введите трёхначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+int num = number / 10 % 10;
+Console.WriteLine(num);
